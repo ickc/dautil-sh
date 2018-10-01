29329baf74f160a242f871839ef20aa89f9c0e5c
Kolen <christian.kolen@gmail.com>
2018-09-30 17:11:02 -0700

    gitignored.sh: improved and cleanup
    also show untracked files

--- bin/gitignored.sh
+++ bin/gitignored.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# find all subdirectories which are git repos, and put to stdout all ignored files.
+
 find -type d -name '\.git' -exec bash -c '
-	for gitDir do
-		inDir="${gitDir%/*}"
-		cd "$inDir"
-		git status --porcelain=2 --ignored | grep "^!" | awk -v inDir="$inDir" "{print inDir \"/\" substr(\$0,3)}"
+	for git do
+		dir="${git::-4}"
+		cd "$dir"
+		git status --porcelain=2 --ignored | grep "^[!?]" | awk -v dir="$dir" "{print dir substr(\$0,3)}"
 		cd - > /dev/null
 	done' bash {} +
